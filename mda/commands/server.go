ba436748ce73cabda8b478b36f59b582fe4caf9d
William Flores <w.f.08@live.com>
2017-07-07 14:58:38 -0700

    Configed the home directory and added showhttpDir flag

--- mda/commands/server.go
+++ mda/commands/server.go
@@ -30,6 +30,7 @@ var (
 	verbose           bool
 	db                *gorm.DB
 	err               error
+	showHTTPDir       bool
 )
 var servercmd = &cobra.Command{
 	Use:     "server",
@@ -43,11 +44,14 @@ func init() {
 	servercmd.Flags().BoolVar(&verbose, "verbose", false, "output log verbose")
 	servercmd.Flags().String("dbname", "sqlite3", "database type")
 	servercmd.Flags().String("connection", "./temp_db.db", "database connection string")
+	servercmd.Flags().String("homedir", "./mda/", "home directory to download into")
+	servercmd.Flags().BoolVar(&showHTTPDir, "httpdir", false, "Output the http directory")
 	//servercmd.Flags().Int("workers", 4, "amount of workers in pool")
 	viper.BindPFlag("port", servercmd.Flags().Lookup("port"))
 	viper.BindPFlag("database.dbname", servercmd.Flags().Lookup("dbname"))
 	viper.BindPFlag("database.connection", servercmd.Flags().Lookup("connection"))
 	viper.BindPFlag("interface.workers", servercmd.Flags().Lookup("workers"))
+	viper.BindPFlag("interface.home", servercmd.Flags().Lookup("homedir"))
 }
 func server(cmd *cobra.Command, args []string) error {
 	if verbose {
@@ -62,36 +66,44 @@ func server(cmd *cobra.Command, args []string) error {
 	//Dispatch = &job.Dispatcher{}
 	//Dispatch.StartDispatcher(viper.GetInt("interface.workers"))
 	db, err = gorm.Open(viper.GetString("database.dbname"), viper.GetString("database.connection"))
+	if verbose {
+		db.LogMode(true)
+	}
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect database \ntype %s with connection %s", viper.GetString("database.dbname"), viper.GetString("database.connection")))
 	}
 	//api.CreateDatabaseTables(db)
-	db.LogMode(true)
-	fmt.Println(db.AutoMigrate(&da.DA{}, &da.Stats{}).GetErrors())
+	if errors := db.AutoMigrate(&da.DA{}, &da.Stats{}).GetErrors(); len(errors) != 0 {
+		fmt.Printf("Cound not auto migrate tables for reasons below %v", errors)
+		fmt.Println()
+		panic("Could not make/migrate tables")
+	}
 	//Dispatch.SetPersistStorage(db)
 	//fmt.Println(db.AutoMigrate(&job.Job{}, &job.JobStats{}).GetErrors())
-	d := da.NewDownloader("G:\\music\\", db)
+	d := da.NewDownloader(viper.GetString("interface.homedir"), db)
 	svc := service.New(db, d)
 	ep := endpoints.New(svc)
 	r := mdahttp.NewHTTPHandler(ep)
-	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		t, err := route.GetPathTemplate()
-		if err != nil {
-			return err
-		}
-		// p will contain regular expression is compatible with regular expression in Perl, Python, and other languages.
-		// for instance the regular expression for path '/articles/{id}' will be '^/articles/(?P<v0>[^/]+)$'
-		p, err := route.GetPathRegexp()
-		if err != nil {
-			return err
-		}
-		m, err := route.GetMethods()
-		if err != nil {
-			return err
-		}
-		fmt.Println(strings.Join(m, ","), t, p)
-		return nil
-	})
+	if verbose || showHTTPDir {
+		r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+			t, err := route.GetPathTemplate()
+			if err != nil {
+				return err
+			}
+			// p will contain regular expression is compatible with regular expression in Perl, Python, and other languages.
+			// for instance the regular expression for path '/articles/{id}' will be '^/articles/(?P<v0>[^/]+)$'
+			p, err := route.GetPathRegexp()
+			if err != nil {
+				return err
+			}
+			m, err := route.GetMethods()
+			if err != nil {
+				return err
+			}
+			fmt.Println(strings.Join(m, ","), t, p)
+			return nil
+		})
+	}
 	log.Infof("Starting Server on port %d", port)
 	server := &http.Server{
 		ReadTimeout:  5 * time.Second,
